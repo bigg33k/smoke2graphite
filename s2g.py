f33f4ca88ebf1824d2cefe1a7f1f82cdaa304a55
Dave Lemaire <lemairemeister@gmail.com>
2016-03-18 17:21:37 +0000

    added statsd metrics

--- s2g.py
+++ s2g.py
@@ -7,51 +7,13 @@ import subprocess
 import socket
 import string
 import ConfigParser
+import statsd
 
-os.nice(15)
+statsd.Connection.set_defaults(host='graphite.bigg33k.net')
+timer = statsd.Timer('smoke2graphite')
+timer2 = statsd.Timer('smoke2graphite')
 
-#---------------------------------------------------------
-def format_timedelta(value, time_format="{days} days, {hours2}:{minutes2}:{seconds2}"):
-
-    if hasattr(value, 'seconds'):
-        seconds = value.seconds + value.days * 24 * 3600
-    else:
-        seconds = int(value)
-
-    seconds_total = seconds
-
-    minutes = int(floor(seconds / 60))
-    minutes_total = minutes
-    seconds -= minutes * 60
-
-    hours = int(floor(minutes / 60))
-    hours_total = hours
-    minutes -= hours * 60
-
-    days = int(floor(hours / 24))
-    days_total = days
-    hours -= days * 24
-
-    years = int(floor(days / 365))
-    years_total = years
-    days -= years * 365
-
-    return time_format.format(**{
-        'seconds': seconds,
-        'seconds2': str(seconds).zfill(2),
-        'minutes': minutes,
-        'minutes2': str(minutes).zfill(2),
-        'hours': hours,
-        'hours2': str(hours).zfill(2),
-        'days': days,
-        'years': years,
-        'seconds_total': seconds_total,
-        'minutes_total': minutes_total,
-        'hours_total': hours_total,
-        'days_total': days_total,
-        'years_total': years_total,
-    })
-
+#os.nice(15)
 
 def get_filepaths(directory):
     """
@@ -60,6 +22,7 @@ def get_filepaths(directory):
     directory in the tree rooted at directory top (including top itself), 
     it yields a 3-tuple (dirpath, dirnames, filenames).
     """
+    timer2.start()
     file_paths = []  # List which will store all of the full filepaths.
 
     # Walk the tree.
@@ -68,10 +31,11 @@ def get_filepaths(directory):
             # Join the two strings in order to form the full filepath.
             filepath = os.path.join(root, filename)
             file_paths.append(filepath)  # Add it to the list.
-
+    timer2.stop('get_filespaths')
     return file_paths  # Self-explanatory.
 #------------------------------------------------------
 
+timer.start()
 start_time = datetime.now()
 print('Started: {}'.format(start_time))
 
@@ -95,6 +59,7 @@ timestamp=0;
 smokestr = []
 smokedata = []
 for f in rrds:
+	filecount=statsd.Counter('smoke2graphite')
 	if f.endswith(".rrd"):
 		host = f.split('/')
 		linesize=len(host)-1
@@ -113,7 +78,7 @@ for f in rrds:
 		smokestr = results[0].split(' ')
 		smokedata = results[2].split(' ')		
 
-		count=0
+		count=0 
 		message=""
 		timestamp=0
 		for str in smokestr:
@@ -150,8 +115,10 @@ for f in rrds:
 				sock.close()
 			message=""
 			count+=1
-		
+	filecount+=1	
 
-
+print 'Files processed: %i' % len(rrds)
 end_time = datetime.now()
 print('Ended: {}'.format(end_time))
+timer.stop('main_loop')
+
